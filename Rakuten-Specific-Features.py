dc580c47e71642df3bcbbc0f2b8e2780c77bc5c0
sachinsk1998 <sachin.kulkarni@amagi.com>
2025-02-08 21:39:53 +0530

    - Implemented Rakuten-Header  Feature

--- Rakuten-Specific-Features.py
+++ Rakuten-Specific-Features.py
@@ -3,5 +3,9 @@ Rakuten Specific Features
 Print Rakuten Header
 """
 print("Module for Rakuten Specific Features")
+from Rakuten_Customer import HEADER
+print(f"Rakuten Headers : {HEADER}")
 
-print("Rakuten Headers : {}")
+print(f"Doing A API Call with {HEADER}")
+print("Got the API Response")
+print("Rendering the Response in UI")
